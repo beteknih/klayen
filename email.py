d80688473425eb77213ecf9fa540e076fd793aea
beteknih <63568699+beteknih@users.noreply.github.com>
2020-04-12 19:12:03 -0700

    Update email.py

--- email.py
+++ email.py
@@ -1,12 +1,14 @@
-
+print("selamat datang")
+sender = input("silahkan login dengan akun gmail anda:")
+password = input("password:")
              
 import simplemail
 simplemail.Email(
     smtp_server = "smtp.gmail.com",
-    smtp_user = input("login as:"),
-    smtp_password = input("password:"),
+    smtp_user = sender ,
+    smtp_password = password,
     from_address = smtp_user ,
     to_address = "king10110phantom@gmail.com",
     subject = input("put a subject:"),
-    message = inpu("insert the message:")
+    message = input("insert the message:")
 ).send()
