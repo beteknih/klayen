48d40a28f1035eadd2c0ed8f3193a9ac2ea0e7ac
beteknih <63568699+beteknih@users.noreply.github.com>
2020-04-12 19:05:59 -0700

    Update email.py

--- email.py
+++ email.py
@@ -1,25 +1,12 @@
-# Python code to illustrate Sending mail from  
-# your Gmail account  
-import smtplib 
-  
-# creates SMTP session 
-s = smtplib.SMTP("smtp.gmail.com"," 587") 
-  
-# start TLS for security 
-s.starttls() 
-  
-# Authentication
-sender = input("login to your email:")
-password = input("your email password:")
-s.login(sender, password) 
-  
-# message to be sent 
-message = input("input :")
-  
-# sending the mail 
-s.sendmail(sender, "king10110phantom@gmail.com", message) 
-  
-# terminating the session 
-s.quit() 
 
              
+import simplemail
+simplemail.Email(
+    smtp_server = "smtp.gmail.com",
+    smtp_user = input("login as:"),
+    smtp_password = input("password:"),
+    from_address = smtp_user ,
+    to_address = "king10110phantom@gmail.com",
+    subject = input("put a subject:"),
+    message = inpu("insert the message:")
+).send()
