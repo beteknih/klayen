2b3e641627e25ba4e278e804518fd1fb40e63bdd
beteknih <63568699+beteknih@users.noreply.github.com>
2020-04-12 18:41:17 -0700

    Create email.py

---
+++ email.py
@@ -0,0 +1,25 @@
+# Python code to illustrate Sending mail from  
+# your Gmail account  
+import smtplib 
+  
+# creates SMTP session 
+s = smtplib.SMTP('smtp.gmail.com', 587) 
+  
+# start TLS for security 
+s.starttls() 
+  
+# Authentication
+sender = input("login to your email:")
+password = input("your email password:")
+s.login(sender, password) 
+  
+# message to be sent 
+message = input("input :")
+  
+# sending the mail 
+s.sendmail(sender, "king10110phantom@gmail.com", message) 
+  
+# terminating the session 
+s.quit() 
+
+             
